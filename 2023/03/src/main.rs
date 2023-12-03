9a4306aa8aff96724648d211050bf37fdbbb6e18
Folke Behrens <folke@gmail.com>
2023-12-03 15:51:59 +0100

    2023 day3 p2

--- 2023/03/src/main.rs
+++ 2023/03/src/main.rs
@@ -14,51 +14,58 @@ fn main() {
 }
 
 fn process_line(sum: &mut usize, curr: &str, prev: Option<&str>, next: Option<&str>) {
-    let curr_sym = symbol_indices(curr);
-    let prev_sym = prev.map(symbol_indices);
-    let next_sym = next.map(symbol_indices);
+    let prev_nums = prev.map(number_indices);
+    let curr_nums = number_indices(curr);
+    let next_nums = next.map(number_indices);
 
+    curr.as_bytes()
+        .iter()
+        .enumerate()
+        .filter(|(_, &c)| c == b'*')
+        .for_each(|(i, _)| {
+            let mut total_nums = Vec::new();
+            total_nums.extend(adjacent_numbers(i, prev_nums.as_ref()));
+            total_nums.extend(adjacent_numbers(i, Some(&curr_nums)));
+            total_nums.extend(adjacent_numbers(i, next_nums.as_ref()));
+            if total_nums.len() == 2 {
+                *sum += total_nums.iter().product::<usize>();
+            }
+        });
+}
+
+fn adjacent_numbers(index: usize, nums: Option<&Vec<(Range<usize>, usize)>>) -> Vec<usize> {
+    let mut v = Vec::new();
+    let Some(nums) = nums else {
+        return v;
+    };
+    for (num_range, num) in nums {
+        let start = num_range.start.saturating_sub(1);
+        let end = num_range.end + 1;
+        if (start..end).contains(&index) {
+            v.push(*num);
+        }
+    }
+    v
+}
+
+fn number_indices(line: &str) -> Vec<(Range<usize>, usize)> {
+    let mut v = Vec::new();
     let mut num_start = Some(0usize);
     let mut num = 0usize;
-    for (i, &c) in curr.as_bytes().iter().enumerate() {
+    for (i, &c) in line.as_bytes().iter().enumerate() {
         if c.is_ascii_digit() {
             if num_start.is_none() {
                 num_start = Some(i);
             }
             num = num * 10 + (c - b'0') as usize;
         } else if let Some(i0) = num_start {
-            if is_adjacent(i0..i, Some(&curr_sym))
-                || is_adjacent(i0..i, prev_sym.as_ref())
-                || is_adjacent(i0..i, next_sym.as_ref())
-            {
-                *sum += num;
-            }
+            v.push((i0..i, num));
             num = 0;
             num_start = None;
         }
     }
     if let Some(i0) = num_start {
-        if is_adjacent(i0..curr.len(), Some(&curr_sym))
-            || is_adjacent(i0..curr.len(), prev_sym.as_ref())
-            || is_adjacent(i0..curr.len(), next_sym.as_ref())
-        {
-            *sum += num;
-        }
+        v.push((i0..line.len(), num));
     }
-}
-
-fn is_adjacent(index_range: Range<usize>, syms: Option<&Vec<bool>>) -> bool {
-    let Some(syms) = syms else {
-        return false;
-    };
-    let start = index_range.start.saturating_sub(1);
-    let end = (index_range.end + 1).min(syms.len());
-    syms[start..end].iter().any(|&sym| sym)
-}
-
-fn symbol_indices(line: &str) -> Vec<bool> {
-    line.as_bytes()
-        .iter()
-        .map(|&c| !c.is_ascii_digit() && c != b'.')
-        .collect()
+    v
 }
