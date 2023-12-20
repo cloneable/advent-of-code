745451787453db8df86997cd7ef16397392af2ea
Folke Behrens <folke@gmail.com>
2023-12-20 01:52:55 +0100

    2023 day18 p2

--- 2023/18/src/main.rs
+++ 2023/18/src/main.rs
@@ -1,161 +1,152 @@
+use std::cmp::Ordering;
+
 fn main() {
   let mut digplan = Vec::new();
+  digplan.push(Default::default());
   std::io::stdin().lines().map(Result::unwrap).for_each(|line| {
     let mut s = line.split_ascii_whitespace();
-    let cmd = s.next().unwrap().chars().next().unwrap();
-    let meters = s.next().unwrap().parse::<isize>().unwrap();
-    let color = s
+    let _ = s.next().unwrap();
+    let _ = s.next().unwrap();
+
+    let hex = s
       .next()
       .unwrap()
       .trim_matches(|c| c == '(' || c == ')' || c == '#')
       .to_owned();
-    digplan.push((cmd, meters, color));
+    let meters = isize::from_str_radix(&hex[0..5], 16).unwrap();
+    let cmd = match hex.as_bytes()[5] {
+      b'0' => 'R',
+      b'1' => 'D',
+      b'2' => 'L',
+      b'3' => 'U',
+      _ => unreachable!(),
+    };
+
+    digplan.push((cmd, meters));
   });
+  digplan[0] = *digplan.last().unwrap();
+  digplan.push(digplan[1]);
 
   let mut digger = (0isize, 0isize);
-  let mut min = (0isize, 0isize);
-  let mut max = (0isize, 0isize);
-  let mut trench = Vec::new();
-  let mut prev = None;
-  let mut lr = (0, 0);
-  trench.push(digger);
-  for (cmd, meters, _) in digplan {
+  let mut hsegments = Vec::new();
+  let mut lr = (0usize, 0usize);
+
+  let mut digs = digplan.windows(3);
+  while let Some([(prev_cmd, _), (cmd, meters), (next_cmd, _)]) = digs.next() {
+    let old_pos = digger;
     match cmd {
-      'U' => {
-        match prev {
-          Some('L') => lr.0 += 1,
-          Some('R') => lr.1 += 1,
-          None => (),
-          _ => unreachable!(),
-        }
-        digger.0 -= meters;
-      }
-      'D' => {
-        match prev {
-          Some('R') => lr.0 += 1,
-          Some('L') => lr.1 += 1,
-          None => (),
-          _ => unreachable!(),
-        }
-        digger.0 += meters;
-      }
+      'U' => digger.0 -= meters,
+      'D' => digger.0 += meters,
+
       'L' => {
-        match prev {
-          Some('D') => lr.0 += 1,
-          Some('U') => lr.1 += 1,
-          None => (),
-          _ => unreachable!(),
-        }
         digger.1 -= meters;
-      }
-      'R' => {
-        match prev {
-          Some('U') => lr.0 += 1,
-          Some('D') => lr.1 += 1,
-          None => (),
+        let adjust = match (prev_cmd, next_cmd) {
+          ('U', 'D') => -1,
+          ('U', 'U') => 0,
+          ('D', 'D') => 0,
+          ('D', 'U') => 1,
           _ => unreachable!(),
-        }
-        digger.1 += meters;
+        };
+        hsegments.push(Segment::new(digger, old_pos, false, adjust));
       }
+
+      'R' => {
+        digger.1 += meters;
+        let adjust = match (prev_cmd, next_cmd) {
+          ('D', 'U') => -1,
+          ('U', 'U') => 0,
+          ('D', 'D') => 0,
+          ('U', 'D') => 1,
+          _ => unreachable!(),
+        };
+        hsegments.push(Segment::new(old_pos, digger, true, adjust));
+      }
+
+      _ => (),
+    }
+
+    match (prev_cmd, cmd) {
+      ('L', 'U') => lr.0 += 1,
+      ('R', 'U') => lr.1 += 1,
+
+      ('R', 'D') => lr.0 += 1,
+      ('L', 'D') => lr.1 += 1,
+
+      ('D', 'L') => lr.0 += 1,
+      ('U', 'L') => lr.1 += 1,
+
+      ('U', 'R') => lr.0 += 1,
+      ('D', 'R') => lr.1 += 1,
+
       _ => unreachable!(),
     }
-    if min.0 > digger.0 {
-      min.0 = digger.0;
-    }
-    if max.0 < digger.0 {
-      max.0 = digger.0;
-    }
-    if min.1 > digger.1 {
-      min.1 = digger.1;
-    }
-    if max.1 < digger.1 {
-      max.1 = digger.1;
-    }
-    prev = Some(cmd);
-    trench.push(digger);
   }
-  assert_eq!((0, 0), *trench.last().unwrap());
-
-  let rows = (1 + max.0 - min.0) as usize;
-  let cols = (1 + max.1 - min.1) as usize;
-  let trench = trench
-    .into_iter()
-    .map(|(row, col)| {
-      let row = (row - min.0) as usize;
-      let col = (col - min.1) as usize;
-      (row, col)
-    })
-    .collect::<Vec<_>>();
-
-  // TODO: try segment tree with U,D lines?
-  let mut ground = vec![vec!['.'; cols]; rows];
-  trench.windows(2).for_each(|w| {
-    let [from, to] = w else {
-      unreachable!();
-    };
-    line(&mut ground, *from, *to);
-  });
+  assert_eq!((0, 0), digger);
 
   let left_is_inside = lr.0 < lr.1;
-
-  let mut area = 0;
-  for r in 0..rows {
-    let mut inside = false;
-    let mut prev = '?';
-    for c in 0..cols {
-      let next = ground[r][c];
-      match next {
-        '.' if inside => {
-          area += 1;
-          ground[r][c] = 'x';
-        }
-        'U' => {
-          area += 1;
-          inside = !left_is_inside
-        }
-        'D' => {
-          area += 1;
-          inside = left_is_inside
-        }
-        'R' | 'L' => area += 1,
-        _ => (),
-      }
-      if prev != next {
-        prev = next;
-      }
+  if !left_is_inside {
+    for seg in hsegments.iter_mut() {
+      seg.subtract = !seg.subtract;
     }
   }
 
-  eprintln!("{area}");
+  hsegments.sort_unstable();
+
+  let mut row = hsegments.first().unwrap().row;
+  let mut area = 0usize;
+  let mut inside = 0usize;
+  for seg in hsegments {
+    assert!(row <= seg.row);
+
+    if row != seg.row {
+      area += (seg.row - row) as usize * inside;
+      row = seg.row;
+    }
+
+    if seg.subtract {
+      inside -= seg.len;
+      area += seg.len; // re-add for current row
+    } else {
+      inside += seg.len;
+    }
+  }
+
+  eprintln!("{area}",);
 }
 
-fn line(ground: &mut Vec<Vec<char>>, from: (usize, usize), to: (usize, usize)) {
-  // TODO: L, R not needed
-  if from.0 == to.0 {
-    if from.1 < to.1 {
-      for c in from.1 + 1..=to.1 {
-        ground[from.0][c] = 'R';
-      }
-    } else if from.1 > to.1 {
-      for c in to.1..from.1 {
-        ground[from.0][c] = 'L';
-      }
-    } else {
-      unreachable!()
-    }
-  } else if from.1 == to.1 {
-    if from.0 < to.0 {
-      for r in from.0..=to.0 {
-        ground[r][from.1] = 'D';
-      }
-    } else if from.0 > to.0 {
-      for r in to.0..=from.0 {
-        ground[r][from.1] = 'U';
-      }
-    } else {
-      unreachable!()
-    }
-  } else {
-    unreachable!()
+#[derive(PartialEq, Eq)]
+struct Segment {
+  row: isize,
+  subtract: bool,
+  len: usize,
+}
+
+impl Segment {
+  fn new(
+    from: (isize, isize),
+    to: (isize, isize),
+    subtract: bool,
+    adjust: isize,
+  ) -> Self {
+    assert_eq!(from.0, to.0);
+    let row = from.0;
+    let len = (from.1.max(to.1) - from.1.min(to.1) + adjust) as usize;
+    Segment { row, len, subtract }
+  }
+}
+
+impl Ord for Segment {
+  fn cmp(&self, other: &Self) -> Ordering {
+    (self.row, self.subtract, self.len).cmp(&(
+      other.row,
+      other.subtract,
+      other.len,
+    ))
+  }
+}
+
+impl PartialOrd for Segment {
+  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+    Some(self.cmp(other))
   }
 }
